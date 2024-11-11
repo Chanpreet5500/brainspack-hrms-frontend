f88550dfb539603da4563aca73a0034b9c458a8b
Sagar Rana <sagarrana8266@gmail.com>
2024-11-11 12:34:24 +0530

    11/11/2024 some changes of sidebar

--- hrms-frontend/src/app/employees/page.tsx
+++ hrms-frontend/src/app/employees/page.tsx
@@ -293,7 +293,18 @@ export default function Employees() {
           recordsPerPage={tableDataLimit}
           page={currentpage}
           onPageChange={(p) => handlePageChange(p)}
-          emptyState={allUserDataLength ? <></> : <></>}
+          emptyState={
+            allUserDataLength ? (
+              <></>
+            ) : (
+              <>
+                {/* <Box p={4} mb={4}>
+                  <IconMoodSad size={36} strokeWidth={1.5} />
+                  No data
+                </Box> */}
+              </>
+            )
+          }
           columns={columns}
         />
       ) : (
